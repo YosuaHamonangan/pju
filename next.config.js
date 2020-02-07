7ecf2a2f08b9a4d1e07c3db8ba6a3fd049d14ddf
Yosua <godbless_yosua@yahoo.co.id>
2020-02-07 22:21:13 +0700

    bug fix

--- next.config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const withCSS = require('@zeit/next-css')
-
-module.exports = {
-	...withCSS(),
-	env: {
-		MAP_API_KEY: process.env.MAP_API_KEY,
-	},
-};
--- server/app.js
+++ server/app.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 var express = require("express");
 var logger = require("morgan");
 var session = require('express-session');
